c2a0517be5193e72589b227a9da249ac0ce551f1
ashkan <ashkan_pirmani@outlook.com>
2021-12-08 16:49:29 +0100

    LG test added

--- 08-LogisticsRegressionTest.py
+++ 08-LogisticsRegressionTest.py
@@ -1,25 +1,28 @@
 import torch
 import torch.nn as nn
-from sklearn import datasets
 import numpy as np
+from sklearn import datasets
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
-import matplotlib.pyplot as plt
-
 
 dataset = datasets.load_breast_cancer()
 
-X,y = dataset.data, dataset.target
+X , y = dataset.data , dataset.target
 
-n_samples , n_feature = X.shape
+n_samples, n_features = X.shape
 
-X_train, X_test ,y_train, y_test = train_test_split(X,y,test_size=0.2,random_state=313)
+print(n_samples, n_features)
+
+
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2,random_state=313)
+
 
 
 sc = StandardScaler()
-
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
+
+
 
 X_train = torch.from_numpy(X_train.astype(np.float32))
 X_test = torch.from_numpy(X_test.astype(np.float32))
@@ -27,42 +30,49 @@ X_test = torch.from_numpy(X_test.astype(np.float32))
 y_train = torch.from_numpy(y_train.astype(np.float32))
 y_test = torch.from_numpy(y_test.astype(np.float32))
 
-
 y_train = y_train.view(y_train.shape[0],1)
 y_test = y_test.view(y_test.shape[0],1)
 
 
-#########################################################
+#############################################################################################
+################################Design the model ############################################
+############################Design the Optimzier and Loss ###################################
+###########################################Training##########################################
+
 
 class LogisticRegression(nn.Module):
     def __init__(self,n_input_features):
         super(LogisticRegression, self).__init__()
         self.linear = nn.Linear(n_input_features,1)
-
-
+    
     def forward(self,X):
         y_hat = torch.sigmoid(self.linear(X))
         return y_hat
 
 
-#########################################################
 
 
 
-model = LogisticRegression(n_feature)
+
+
+
+model = LogisticRegression(n_features)
+
+
+learning_rate = 0.001
+n_iters = 1000
+
 
 loss = nn.BCELoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
 
-optimizer = torch.optim.SGD(model.parameters(), lr=0.01)
-
-
-n_iters = 1000
 
 
 for epoch in range(n_iters):
 
     y_hat = model(X_train)
-    l = loss(y_hat, y_train)
+
+    l = loss(y_hat,y_train)
 
     l.backward()
 
@@ -70,8 +80,11 @@ for epoch in range(n_iters):
 
     optimizer.zero_grad()
 
+
     if epoch % 100 == 0:
-        print(f'epoch{epoch+1} , loss: {l}')
+
+        [w,b] = model.parameters()
+        print(f'epoch: {epoch+1}, loss: {l}')
 
 
 with torch.no_grad():
