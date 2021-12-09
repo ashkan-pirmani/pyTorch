5ce01faf1c63610d24ba49ee9b240fbb4300e903
ashkan <ashkan_pirmani@outlook.com>
2021-12-09 11:32:55 +0100

    transform

---
+++ 10-transform.py
@@ -0,0 +1,43 @@
+import torch
+import torchvision
+from torch.utils.data import Dataset
+import numpy as np
+
+
+class WineDataset(Dataset):
+
+    def __init__(self , transform=None):
+        xy = np.loadtxt('./wine.csv',dtype=np.float32,delimiter=',',skiprows=1)
+        self.n_samples = xy.shape[0]
+
+        self.x = xy[:,1:]
+        self.y = xy[:,[0]]
+
+        self.transform = transform
+
+
+    def __getitem__(self, index):
+        samples = self.x[index] , self.y[index]
+        if self.transform is not None:
+            samples = self.transform(samples)
+            return samples
+
+    def __len__(self):
+        return self.n_samples
+
+
+class ToTensor:
+    def __call__(self, samples):
+        inputs , labels = samples
+        return torch.from_numpy(inputs), torch.from_numpy(labels)
+
+
+
+dataset = WineDataset(transform=ToTensor())
+dataset1 = WineDataset()
+
+
+print(type(dataset))
+
+
+
